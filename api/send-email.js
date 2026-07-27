73b424d70a4aceaf2313091f5f70118dd817121c
Web Creater <84850386+harshilarora250@users.noreply.github.com>
2026-07-28 00:16:20 +0400

    Set template for email

--- api/send-email.js
+++ api/send-email.js
@@ -1,6 +1,53 @@
 import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
+//Email thing
+const emailHTML = `
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    background-color: #f0f1f5;
+    font-family: Arial, Helvetica, sans-serif;
+}
+
+.container {
+    max-width: 600px;
+    margin: 40px auto;
+    background: #e1e6e8;
+    padding: 40px 30px;
+    text-align: center;
+    border-radius: 12px;
+    color: #05375a;
+}
+
+h1 {
+    font-size: 32px;
+}
+
+p {
+    font-size: 16px;
+    line-height: 1.5;
+}
+</style>
+</head>
+
+<body>
+<div class="container">
+    <h1>Thanks for signing up!</h1>
+
+    <p>
+        Welcome to SplitIt. We're excited to have you with us.
+        You'll receive updates and important information soon.
+    </p>
+</div>
+</body>
+</html>
+`;
+//email thing ends
 
 export default async function handler(req, res) {
     if (req.method !== "POST") {
@@ -13,8 +60,8 @@ export default async function handler(req, res) {
         const { data, error } = await resend.emails.send({
             from: "SplitIt <onboarding@resend.dev>", // change later to your verified domain
             to: email,
-            subject: "Verify your email",
-            html: "<strong>Click here to verify!</strong>",
+            subject: "Thanks for signing up for emails!",
+            html: emailHtml,
         });
 
         if (error) {
